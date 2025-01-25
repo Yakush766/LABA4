e90bf3ef19bcc98e5543fb752cd3b34358c90b67
Yakush766 <anton.yakushenko72@gmail.com>
2025-01-25 13:14:50 +0300

    Добавление файлов

---
+++ individual.py
@@ -0,0 +1,7 @@
+volume = float(input("Enter the volume of the object (in cubic meters): "))
+mass = float(input("Enter the mass of the object (in kilograms): "))
+
+density = mass / volume
+
+print(f"The density of the material is: {density:.2f} kg/m³")
+
